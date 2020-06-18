ab151b000c85061d10928c27b2f599ef41ece90f
Pasindu Thiwanka <pasindu.17@itfac.mrt.ac.lk>
2020-06-18 16:23:59 +0530

    Intial Commit

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,10 +2,17 @@ import {Component} from "@angular/core";
 
 @Component({
   selector :'app-server',
-  templateUrl:'./server.componnent.html'
+  templateUrl:'./server.componnent.html',
+  styleUrls:['./server.component.css']
 })
 
 export class ServerComponent{
+  serverID =10;
+  serverStatues ='offline';
+
+  getServerStaues(){
+    return this.serverStatues;
+  }
 
 }
 
